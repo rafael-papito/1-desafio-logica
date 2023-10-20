2ed24fc56560f3b623f91b98d3f6c1ff81c95b11
Rafael Porto <karlajaqueline@Mac-do-Papito.local>
2023-10-20 10:44:59 -0300

    Mudando o nome da let

--- logica.js
+++ logica.js
@@ -1,20 +1,20 @@
-let nameHeroi = "Lokura"
+let nameHero = "Lokura"
 let xp = 8000
 
 if (xp < 1000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Ferro (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Ferro (" + xp + "xp)")
 }else if (xp >= 1001, xp <= 2000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Bronze (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Bronze (" + xp + "xp)")
 }else if (xp >= 2001, xp <= 5000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Prata (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Prata (" + xp + "xp)")
 }else if (xp >= 6001, xp <= 7000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Ouro (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Ouro (" + xp + "xp)")
 }else if (xp >= 7001, xp <= 8000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Platina (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Platina (" + xp + "xp)")
 }else if (xp >= 8001, xp <= 9000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Acendente (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Acendente (" + xp + "xp)")
 }else if (xp >= 9001, xp <= 10000){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Imortal (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Imortal (" + xp + "xp)")
 }else if (xp >= 10001){
-    console.log("O Herói de nome " + nameHeroi + " está no nível Radiante (" + xp + "xp)")
+    console.log("O Herói de nome " + nameHero + " está no nível Radiante (" + xp + "xp)")
 }
